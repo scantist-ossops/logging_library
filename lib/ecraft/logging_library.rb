a3241871f6037e04e81650cfbb0bc7ba52e12fa4
Per Lundberg <per.lundberg@ecraft.com>
2017-05-18 12:41:17 +0300

    ecraft/logging_library: Must require 'logging_library' to provide compatibility.

--- lib/ecraft/logging_library.rb
+++ lib/ecraft/logging_library.rb
@@ -1,3 +1,5 @@
+require 'logging_library'
+
 module Ecraft
   # For backwards compatibility, since this library used to be called ecraft-logging_library before it was released as FOSS.
   LoggingLibrary = ::LoggingLibrary
--- spec/ecraft/logging_library_spec.rb
+++ spec/ecraft/logging_library_spec.rb
@@ -5,4 +5,8 @@ describe Ecraft::LoggingLibrary do
   it 'is defined as a compatibility module' do
     expect(subject).to eq LoggingLibrary
   end
+
+  it 'contains a Loggable constant' do
+    expect { Ecraft::LoggingLibrary::Loggable }.to_not raise_error
+  end
 end
